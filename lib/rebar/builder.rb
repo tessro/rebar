fe293f29d573fb5a66ba9a01ca4077b7c49b1602
Paul Rosania <paul.rosania@gmail.com>
2012-02-16 19:30:10 -0800

    Track some performance stats

--- lib/rebar/builder.rb
+++ lib/rebar/builder.rb
@@ -5,9 +5,20 @@ module Rebar
     end
 
     attr_reader :connection
+    attr_reader :system_cpu_time
+    attr_reader :user_cpu_time
+    attr_reader :memory_delta
 
     def build(&block)
+      initial_cpu_sys  = connection.info['used_cpu_sys'].to_i
+      initial_cpu_user = connection.info['used_cpu_user'].to_i
+      initial_memory   = connection.info['used_memory'].to_i
+
       Rebar::DSL::Database.new(connection).instance_eval(&block)
+
+      @system_cpu_time = connection.info['used_cpu_sys'].to_i  - initial_cpu_sys
+      @user_cpu_time   = connection.info['used_cpu_user'].to_i - initial_cpu_user
+      @memory_delta    = connection.info['used_memory'].to_i   - initial_memory
     end
 
     def clean!
